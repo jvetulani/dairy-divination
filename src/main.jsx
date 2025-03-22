4d2d5446c7198a6e789b16684bcc682eb1d1a593
Joanna Vetulani <j.vetulani@lucidrealitylabs.com>
2025-03-22 01:15:50 +0100

    Initial commit for cheesy enlightenment

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import DairyDivination from './App.jsx'; // Importing your App.js component
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <DairyDivination />
+  </React.StrictMode>
+);
